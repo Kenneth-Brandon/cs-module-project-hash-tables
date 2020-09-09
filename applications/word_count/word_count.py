f0119c0632cc89e695e571b2fbc9ced77103433f
unknown <ken.brandon@aol.com>
2020-09-08 17:43:05 -0700

    worked through half of the applications

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,27 @@
 def word_count(s):
     # Your code here
+    cache = {}
+    skip_characters = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split(" ")
+    lowercase_s = s.lower()
 
+# replace skipped characters with empty space
+    for char in skip_characters:
+        lowercase_s = lowercase_s.replace(char, "")
+
+    for word in lowercase_s.split():
+        if word == "":
+            continue
+        if word not in cache:
+            cache[word] = 1
+        else:
+            cache[word] += 1
+
+    return cache
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
